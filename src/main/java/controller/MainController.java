0f9eb6ed0b352cfb5c81123ff21ef5e756c6661d
changgn <kjs_lovey@naver.com>
2016-06-13 18:30:32 +0900

    main수정

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -68,6 +68,19 @@ public class MainController {
 			//모든 게시글을 가져온다
 			boardList = boardDao.getList();
 			
+		}else { // 로그인 된 경우
+			// 로그인 한 아이디의 카테고리 정보를 가져온다
+			//categoryIdList = membersCategoryDao.getCategoryIdById(id);
+			
+			if(categoryIdList.size() == 0){ // 로그인 한 아이디의 카테고리 정보가 없을 경우
+				
+				//모든 게시글을 가져온다
+				boardList = boardDao.getList();
+			} else { // 로그인 한 아이디의 카테고리 정보가 있을 경우
+				
+				//카테고리 정보에 따른 게시글을 가져온다
+				//boardList = boardDao.getListByCategoryId(categoryIdList);
+			}
 		}
 		if(boardList!=null)	{
 			for(BoardCommand vo : boardList) {
