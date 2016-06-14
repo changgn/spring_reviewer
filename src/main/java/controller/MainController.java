2a970fd52e399bdb60581c2cf8f5a826a6081844
changgn <kjs_lovey@naver.com>
2016-06-14 20:00:01 +0900

    메인

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -57,43 +57,40 @@ public class MainController {
 	@RequestMapping("/main/main.do")
 	public String main(HttpServletRequest request, HttpServletResponse response, Model model){
 		
-		String id = (String)request.getSession().getAttribute("id"); // 濡쒓렇�씤 �븳 id
-		String login_status = (String)request.getSession().getAttribute("login_status");	//濡쒓렇�씤 �긽�깭
+		String id = (String)request.getSession().getAttribute("id"); 
+		String login_status = (String)request.getSession().getAttribute("login_status");
 		
 		List<BoardCommand> boardList = null;
 		List<HashMap> allBoardList = new ArrayList<HashMap>();
 		List<String> categoryIdList = null;
 		
 		if(login_status==null){
-			login_status = "2";	// 濡쒓렇�씤 �븞�맂 �긽�깭
+			login_status = "2";
 			request.getSession().setAttribute("login_status", login_status);
 		}
-		if(login_status.equals("2")){ // 濡쒓렇�씤 �븞�맂 寃쎌슦
+		if(login_status.equals("2")){
 			
-			//紐⑤뱺 寃뚯떆湲��쓣 媛��졇�삩�떎
 			boardList = boardDao.getList();
 			
-		}else { // 濡쒓렇�씤 �맂 寃쎌슦
-			// 濡쒓렇�씤 �븳 �븘�씠�뵒�쓽 移댄뀒怨좊━ �젙蹂대�� 媛��졇�삩�떎
+		}else {
+			
 			categoryIdList = memberCategoryDao.getCategoryIdById(id);
 			
-			if(categoryIdList.size() == 0){ // 濡쒓렇�씤 �븳 �븘�씠�뵒�쓽 移댄뀒怨좊━ �젙蹂닿� �뾾�쓣 寃쎌슦
+			if(categoryIdList.size() == 0){
 				
-				//紐⑤뱺 寃뚯떆湲��쓣 媛��졇�삩�떎
 				boardList = boardDao.getList();
-			} else { // 濡쒓렇�씤 �븳 �븘�씠�뵒�쓽 移댄뀒怨좊━ �젙蹂닿� �엳�쓣 寃쎌슦
+			} else { 
 				
-				//移댄뀒怨좊━ �젙蹂댁뿉 �뵲瑜� 寃뚯떆湲��쓣 媛��졇�삩�떎
 				boardList = boardDao.getListByCategoryId(categoryIdList);
 			}
 		}
 		if(boardList!=null)	{
 			for(BoardCommand vo : boardList) {
 				HashMap<String, Object> boardMap = new HashMap<String, Object>();
-				//PhotoCommand photo = photoDao.getOneByBoardNum(vo.getBoard_num());
+				PhotoCommand photo = photoDao.getOneByBoardNum(vo.getBoard_num());
 				CategoryCommand category = categoryDao.getOne(vo.getCategory_id());
-				//String commentCount = commentDao.getCountByBoardNum(vo.getBoard_num());
-				//if(commentCount==null)	commentCount="0";
+				String commentCount = commentDao.getCountByBoardNum(vo.getBoard_num());
+				if(commentCount==null)	commentCount="0";
 				boolean contentFlag = false;
 				String[] contentSub = vo.getContent().split("\n");
 				if(contentSub.length > 3) {
@@ -101,9 +98,9 @@ public class MainController {
 					vo.setContent(contentSub[0] + contentSub[1] + contentSub[2]);
 				}
 				boardMap.put("board", vo);
-				//boardMap.put("photo", photo);
+				boardMap.put("photo", photo);
 				boardMap.put("category", category);
-				//boardMap.put("commentCount", commentCount);
+				boardMap.put("commentCount", commentCount);
 				boardMap.put("contentFlag", contentFlag);
 				allBoardList.add(boardMap);
 			}
