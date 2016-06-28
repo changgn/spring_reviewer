37b600b0eb567fc8fab6d15d90a325c1a2b1c5a7
KJW <eleven5433@gmail.com>
2016-06-28 18:03:58 +0900

    수정

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import java.net.CookieStore;
 import java.util.HashMap;
 
 import javax.servlet.http.Cookie;
@@ -9,14 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import dao.MemberDAO;
 import command.MemberCommand;
@@ -129,7 +125,7 @@ public class MemberController {
 	@RequestMapping(value="/member/modify.do",method=RequestMethod.POST)
 	public String submit(MemberCommand memberInfo) {
 		
-		int updateSuccess = memberDao.modifyPro(memberInfo);
+		memberDao.modifyPro(memberInfo);
 		
 		return "redirect:/main/main.do";
 	}
--- src/main/webapp/WEB-INF/views/layout/layout.jsp
+++ src/main/webapp/WEB-INF/views/layout/layout.jsp
@@ -24,7 +24,7 @@
 					$("#nav").css("position", "static");
 				}
 			});
-
+			
 		});
 		$(function(){		
 			
